93ac20fa1211cad6f0ca38520061f2d4c8c81919
taurus366 <47596673+taurus366@users.noreply.github.com>
2020-10-31 19:07:37 +0200

    JPA delete,persist(add)

--- src/main/java/course/springdate/jpaintro/JpaIntroMain.java
+++ src/main/java/course/springdate/jpaintro/JpaIntroMain.java
@@ -12,22 +12,41 @@ public class JpaIntroMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("school_jpa");
         EntityManager em = emf.createEntityManager();
         Student student = new Student("Gimitar Pavlov");
+
+        // CREATE new student into DB
         em.getTransaction().begin();
         em.persist(student);
         em.getTransaction().commit();
 
         em.getTransaction().begin();
-        Student student1 = em.find(Student.class, 1L);
-        System.out.println(student1.getName() + student1.getRegistrationDate());
+        Student student1 = em.find(Student.class, 4L);
         em.getTransaction().commit();
 
+
+
+        // REMOVE student from DB
+//        em.getTransaction().begin();
+//        em.remove(student1);
+//        em.getTransaction().commit();
+
+//        em.getTransaction().begin();
+//        Student student1 = em.find(Student.class, 1L);
+//        System.out.println(student1.getName() + student1.getRegistrationDate());
+//        em.getTransaction().commit();
+
+//        em.getTransaction().begin();
+//        em.createQuery("SELECT s FROM Student s WHERE s.name LIKE ?1")
+//                .setParameter(1, "G%")
+//                .getResultList().forEach(System.out::println);
+//
+//        em.getTransaction().commit();
+
+
+        // UPDATE EXISTING ROW student in DB
         em.getTransaction().begin();
-        em.createQuery("SELECT s FROM Student s WHERE s.name LIKE ?1")
-                .setParameter(1, "G%")
-                .getResultList().forEach(System.out::println);
-
+        student1.setName("ALI ZINAL");
+       // em.merge(student1);
         em.getTransaction().commit();
-
         em.close();
 
     }
